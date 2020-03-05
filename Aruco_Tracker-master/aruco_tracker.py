1e96ffcd8080d23c9c3d265d3a38942900333b79
Ilias <iliasamri00@gmail.com>
2020-03-05 12:20:24 +0100

    [func]Added comments and flag for the GUI

--- Aruco_Tracker-master/aruco_tracker.py
+++ Aruco_Tracker-master/aruco_tracker.py
@@ -7,6 +7,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 
+flagShowWindow = False;
 #cap = cv2.VideoCapture(0)
 camera =PiCamera()
 camera.resolution=(640,480)
@@ -14,10 +15,8 @@ camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(640,480))
 time.sleep(0.1)
 
-#frame = cv2.imread("a.jpg")
 
 
-####---------------------- CALIBRATION ---------------------------
 # termination criteria for the iterative algorithm
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -41,30 +40,9 @@ cv_file = cv2.FileStorage("calib_images/test.yaml", cv2.FILE_STORAGE_READ)
 # FileNode object back instead of a matrix
 mtx = cv_file.getNode("camera_matrix").mat()
 dist = cv_file.getNode("dist_coeff").mat()
-'''
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    # find the chess board (calibration pattern) corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6),None,flags = cv2.CALIB_CB_FAST_CHECK)
-
-    # if calibration pattern is found, add object points,
-    # image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-
-        # Refine the corners of the detected corners
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
-
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
-
-
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-'''
+#This part is not done because it's ressource heavy and not needed since it's already done and stored in afile calib_images/test.yaml
 ###------------------ ARUCO TRACKER ---------------------------
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # operations on the frame
     rawCapture.truncate(0)
@@ -97,33 +75,37 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         for i in range(0, ids.size):
             # draw axis for the aruco markers
             aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
-            nptvec = np.array(tvec[i])
-            if ids[i] == 2 and nptvec[0,2]<2:
-                print("RED !!!!!!!")
-#            print("Vecteur de rotation :")
-#            print(rvec[i])
-#            print("Vecteur de translation :")
-#            print(tvec[i])
-        # draw a square around the markers
-        aruco.drawDetectedMarkers(frame, corners)
+            nptvec = np.array(tvec[i])#We get Translation vector of the marker of the index i
+            if ids[i] == 2 and nptvec[0,2]<2:#We now check if the index is the second, and check if the translation value is smaller than 2, which is an arbitrary value
+                #TODO : When calibrating we should check if the index is the correct one, in the line above, and check if the range (nptvect[0,2]) is good
+                print("RED !!!!!!!")#This should be replaced by a blinking LED
+#            print(rvec[i])#
+#            print("Vecteur de rotation :"#TODO : Logs the values, Needed to know which value is needed for calibration
+#            print("Vecteur de translation :")#Same as precedent
+#            print(tvec[i])#Same as precedent
+        # draw a square around the markers#Same as precedent
+        aruco.drawDetectedMarkers(frame, corners)#Draws the aruco three base vectors on the marker 
 
 
+      
         # code to show ids of the marker found
         strg = ''
         for i in range(0, ids.size):
             strg += str(ids[i][0])+', '
 
-        cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
-#        print(str(ids[i][0]))
+            cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+#           print(str(ids[i][0]))
 
     else:
         # code to show 'No Ids' when no markers are found
-#        print("no Ids")
+#       print("no Ids")
         cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
 
-    # display the resulting frame
+        # display the resulting frame
+               
     frame = cv2.resize(frame, (640, 480))                    # Resize image
-    cv2.imshow('frame',frame)
+    if flagShowWindow == True :
+        cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
